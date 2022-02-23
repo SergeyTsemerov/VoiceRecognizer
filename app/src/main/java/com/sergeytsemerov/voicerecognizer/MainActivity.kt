581385ff318bd745f5ae5e8efc9299152ab33a46
SergeyTsemerov <sergeytsemerov@gmail.com>
2022-02-23 03:30:13 +0300

    Added: speech to text recognizer

--- app/src/main/java/com/sergeytsemerov/voicerecognizer/MainActivity.kt
+++ app/src/main/java/com/sergeytsemerov/voicerecognizer/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.sergeytsemerov.voicerecognizer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.sergeytsemerov.voicerecognizer.databinding.ActivityMainBinding
 import java.util.*
 
@@ -18,19 +24,54 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    private val launchActivity =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val spokenText =
+                    result.data?.let { arrayListOf(it.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)) }
+                if (spokenText != null) {
+                    binding.editText.setText(spokenText[0].toString().removeSurrounding(PREFIX_BRACE, SUFFIX_BRACE))
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        model.init(textToSpeech)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            checkForPermission()
+        }
+        model.init(textToSpeech, launchActivity)
         with(binding) {
             playButton.setOnClickListener {
                 val text = editText.text?.trim().toString()
                 model.textSpeaker(text.ifEmpty {
-                    "There is NO text!"
+                    EMPTY_TEXT_INPUT
                 })
             }
+            voiceButton.setOnClickListener {
+                model.showSpeechRecognizer()
+            }
         }
+    }
+
+    private fun checkForPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            RECORD_AUDIO_REQUEST_CODE
+        )
+    }
+
+    companion object {
+        const val RECORD_AUDIO_REQUEST_CODE = 1
+        const val EMPTY_TEXT_INPUT = "There is NO text!"
+        const val PREFIX_BRACE = "["
+        const val SUFFIX_BRACE = "]"
     }
 }
--- app/src/main/java/com/sergeytsemerov/voicerecognizer/MainViewModel.kt
+++ app/src/main/java/com/sergeytsemerov/voicerecognizer/MainViewModel.kt
@@ -1,6 +1,9 @@
 package com.sergeytsemerov.voicerecognizer
 
+import android.content.Intent
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
+import androidx.activity.result.ActivityResultLauncher
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
@@ -8,14 +11,34 @@ import kotlinx.coroutines.launch
 class MainViewModel : ViewModel() {
 
     private lateinit var textToSpeech: TextToSpeech
+    private lateinit var startForResult: ActivityResultLauncher<Intent>
 
     fun init(
-        engine: TextToSpeech
+        engine: TextToSpeech,
+        launcher: ActivityResultLauncher<Intent>
     ) = viewModelScope.launch {
         textToSpeech = engine
+        startForResult = launcher
     }
 
     fun textSpeaker(text: String) = viewModelScope.launch {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, EMPTY_STRING)
+    }
+
+    fun showSpeechRecognizer() {
+        startForResult.launch(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            putExtra(RecognizerIntent.EXTRA_LANGUAGE, LANGUAGE_DEFAULT)
+            putExtra(RecognizerIntent.EXTRA_PROMPT, EXTRA_PROMPT_VALUE)
+        })
+    }
+
+    companion object {
+        const val LANGUAGE_DEFAULT = "en-US"
+        const val EXTRA_PROMPT_VALUE = "Speak Now"
+        const val EMPTY_STRING = ""
     }
 }
